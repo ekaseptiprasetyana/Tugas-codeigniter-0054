e7e93c1aa5261e939b5afcf227b63d29c6e0a3dc
ekaseptiprasetyana <ekaseptiprasetyana@gmail.com>
2024-12-08 16:37:31 +0600

    first commit

---
+++ app/Views/DataMahasiswa.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Profil</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container py-3">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card shadow">
+                    <div class="card-header text-center bg-success text-white">
+                        <h4>Profil Saya</h4>
+                    </div>
+                    <div class="card-body">
+                    <div class="row mb-3">
+                        <div class="col-md-2 font-weight-bold">Nama</div>
+                        <div class="col-md-8">: Eka Septi Prasetyana</div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-md-2 font-weight-bold">NIM</div>
+                        <div class="col-md-8">: 23.240.0054</div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-md-2 font-weight-bold">Kelas</div>
+                        <div class="col-md-8">: 3P41</div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-md-2 font-weight-bold">Jurusan</div>
+                        <div class="col-md-8">: Teknik Informatika</div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-md-2 font-weight-bold">Jenjang</div>
+                        <div class="col-md-8">: S1</div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="text-black text-center mt-1">
+        <p>&copy; Eka Septi Prasetyana - 23.240.0054.</p>
+    </footer>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
